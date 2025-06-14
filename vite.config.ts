b48b79318d3d13ef8f2c3d7e72e889f6c7827de8
Anna Fistarol <anna.fistarol@watch.tv.br>
2025-06-14 17:52:38 -0300

    feat: update TypeScript configuration and Vite aliasing for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@utils': '/src/utils',
+      '@types': '/src/types',
+      '@store': '/src/store',
+      '@styles': '/src/styles',
+      '@pages': '/src/pages',
+    },
+  },
 })
