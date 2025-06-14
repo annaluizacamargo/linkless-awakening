b138edb738e05cd6495805ced45bfb4f9d7ac751
Anna Fistarol <anna.fistarol@watch.tv.br>
2025-06-14 17:49:35 -0300

    feat: add Header component with links and styling for improved navigation

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,50 @@
+import { Divider, Link } from '@mui/material'
+import { HeaderOptions } from '@utils/constants/HeaderOptions'
+import { stringToMd5 } from '@utils/helpers/String.helper'
+import type { HeaderLink } from '@utils/constants/HeaderOptions'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import GoldLogo from '@assets/logo/linkless-awakening-logo-gold.svg'
+import React from 'react'
+import './styles.css'
+
+/**
+ * Header component
+ * @returns {JSX.Element} Header component
+ */
+export default function Header(): React.JSX.Element {
+  return (
+    <header className="site-header">
+      <Box>
+        <Link href="/">
+          <img src={GoldLogo} alt="Linkless Awakening Logo" className="logo" height={4} />
+        </Link>
+
+        <Container className="site-header-container">
+          {HeaderOptions.links.map((link: HeaderLink, index: number) => (
+            <React.Fragment key={stringToMd5(link.path + 'section')}>
+              <Link
+                key={stringToMd5(index + link.path)}
+                href={link.path}
+                color="inherit"
+                underline="none"
+                className={HeaderOptions.getLinkClass(link.path)}
+              >
+                {link.icon} {link.label}
+              </Link>
+
+              {index < HeaderOptions.links.length - 1 && (
+                <Divider
+                  orientation="vertical"
+                  flexItem
+                  sx={{ backgroundColor: '#212732', width: '2px' }}
+                  key={stringToMd5('divider' + index)}
+                />
+              )}
+            </React.Fragment>
+          ))}
+        </Container>
+      </Box>
+    </header>
+  )
+}
---
+++ src/utils/constants/HeaderOptions.ts
@@ -0,0 +1,33 @@
+/**
+ * HeaderOptions.ts
+ * This file contains the configuration for the header links in the application.
+ */
+export const HeaderOptions: HeaderOptionsType = {
+  links: [
+    {
+      label: 'Cadastro de Monstros',
+      path: '/cadastro',
+      icon: '📋',
+    },
+    {
+      label: 'Arena de Batalha',
+      path: '/batalha',
+      icon: '⚔️',
+    },
+  ],
+  getLinkClass: (path: string): string => {
+    const currentPath = window.location.pathname
+    return currentPath === path ? 'site-header-link active' : 'site-header-link'
+  },
+}
+
+export type HeaderLink = {
+  label: string
+  path: string
+  icon: string
+}
+
+type HeaderOptionsType = {
+  links: HeaderLink[]
+  getLinkClass: (path: string) => string
+}
