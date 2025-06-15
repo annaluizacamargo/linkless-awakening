6a3eb9631a705706b7f3aa4e8f3a819749ddea65
Anna Fistarol <anna.fistarol@watch.tv.br>
2025-06-15 17:48:18 -0300

    refactor: simplify routing by replacing construction placeholders with actual components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Box, Container } from '@mui/material'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import GoldLogo from '@assets/logo/linkless-awakening-logo-gold.svg'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import NotFoundPage from '@pages/404'
+import MonstersForm from '@pages/cadastro'
+import BattleArena from '@pages/batalhas'
 import './App.css'
 
 /**
  * Linkless Awakening App
  * @description This is the main component of the Linkless Awakening application.
  */
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="app">
@@ -18,46 +19,17 @@ function App() {
 
         <Container className="main-container" maxWidth="lg">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Box>
-                  <img src={GoldLogo} alt="Linkless Awakening Logo" className="logo" />
-                  <h1 className="construction-title">🏗️ Site em Construção...</h1>
-                </Box>
-              }
-            />
-
-            {/* <Route path="/cadastro" element={<AddNewMonsterPage />} /> */}
-            <Route
-              path="/cadastro"
-              element={
-                <Box>
-                  <img src={GoldLogo} alt="Linkless Awakening Logo" className="logo" />
-                  <h1 className="construction-title">🏗️ Site em Construção...</h1>
-                </Box>
-              }
-            />
-
-            {/* <Route path="/batalhas" element={<BattlesPage />} /> */}
-            <Route
-              path="/batalhas"
-              element={
-                <Box>
-                  <img src={GoldLogo} alt="Linkless Awakening Logo" className="logo" />
-                  <h1 className="construction-title">🏗️ Site em Construção</h1>
-                </Box>
-              }
-            />
-
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/" element={<MonstersForm />} /> {/* Default route to MonstersForm [Monsters registration] */}
+            <Route path="/cadastro" element={<MonstersForm />} /> {/* Monsters registration page */}
+            <Route path="/batalhas" element={<BattleArena />} /> {/* Battle arena page */}
+            <Route path="*" element={<NotFoundPage />} /> {/* 404 Not Found page */}
           </Routes>
         </Container>
+
         <Footer />
+
         <Box className="background-box" />
       </div>
     </Router>
   )
 }
-
-export default App
